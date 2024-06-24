6eb74eb9ba3bfa350b8d3c0e27ed286dd28f2c87
Yehor Kardash <34182728+Goganoid@users.noreply.github.com>
2024-06-24 13:43:55 +0300

    Update archive (#3)
    * Update archive and gitignore
    
    * Untrack data.js
    
    ---------
    
    Co-authored-by: YehorKardashRadency <yehor.k@radency.com>

--- src/data/background/data.js
+++
@@ -1,5 +0,0 @@
-const listing = require("../background/listing.json");
-const filters = require("../background/filters.json");
-const itemDisplayConfig = require("./itemDisplayConfig.json");
-const data = require('./data.json');
-export {listing,filters,itemDisplayConfig,data}
--- src/data/trap/data.js
+++
@@ -1,5 +0,0 @@
-const listing = require("./listing.json");
-const filters = require("./filters.json");
-const itemDisplayConfig = require("./itemDisplayConfig.json");
-const data = require('./data.json');
-export {listing,filters,itemDisplayConfig,data}
